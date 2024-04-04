2992fd32194de029df046d99b07956e97eeb0d45
John Serrano <andrey_201015@hotmail.com>
2024-04-04 15:49:30 -0500

    Add font noto sans

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans Variable', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        geeparts: '#ffcf2c',
+      },
+    },
+  },
+  plugins: [],
 }
