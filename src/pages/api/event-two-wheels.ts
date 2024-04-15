0f8e198c4adb3617c37e181ee4f2c796731c280b
John Serrano <andrey_201015@hotmail.com>
2024-04-15 16:51:39 -0500

    Add interface and listId prop

--- src/pages/api/event-two-wheels.ts
+++ src/pages/api/event-two-wheels.ts
@@ -10,13 +10,24 @@ const res = (
   { status, statusText, headers }: { status?: number; statusText?: string; headers?: Record<string, string> }
 ) => new Response(JSON.stringify(body), { status, statusText, headers })
 
+interface FormData {
+  email: string
+  first_name: string
+  last_name: string
+  phone: string
+  motorcycle: string
+  type_person: string
+  city: string
+  listId: string
+}
+
 export const POST: APIRoute = async ({ request }) => {
   const url = 'https://api.brevo.com/v3/contacts'
   const apiKey = import.meta.env.API_KEY
 
   if (request.headers.get('Content-Type') === 'application/json') {
-    const formData = await request.json()
-    const { email, first_name, last_name, phone, motorcycle, type_person, city } = formData
+    const formData: FormData = await request.json()
+    const { email, first_name, last_name, phone, motorcycle, type_person, city, listId } = formData
 
     if (!email) {
       return res({ message: 'Email is required' }, { status: 400 })
@@ -34,7 +45,7 @@ export const POST: APIRoute = async ({ request }) => {
         ...(city && { CITY: city }),
         WHATSAPP: `+57${phone}`,
       },
-      listIds: [4],
+      listIds: [Number(listId)],
       emailBlacklisted: false,
       smsBlacklisted: false,
       updateEnabled: false,
